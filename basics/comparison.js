3c692b87c93a2e630e0705e09e975212b631fece
tarun1299 <152164072+tarun1299@users.noreply.github.com>
2023-11-27 14:56:35 +0000

    comparison operator js

---
+++ basics/comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); // false
+console.log(null == 0); // false 
+console.log(null >= 0);  // true why it is change null to 0 and 0 is equal 0
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false 
+console.log(undefined < 0); // false
+
+// === 
+
+console.log("2" === 2); // false -> when we use === operator it is check accurate  like value with its data type  so fisrt 2 is string and second is number sp it is not equal 
