d1218ed36d9f1959db23b02537d81a209bec656b
tarun1299 <152164072+tarun1299@users.noreply.github.com>
2023-11-27 13:43:27 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,80 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+//==============================================================
+let marks = "48"
+//console.log(typeof marks);
+//console.log(marks, typeof(marks)); // check the type of variable using method 
+let changeNumber = Number(marks)
+let bool  = Boolean(marks)
+//console.log(changeNumber, typeof changeNumber);
+//console.log(bool, typeof bool); // output-> true, boolean
+
+let flag = true
+let q = Number(flag)
+let d = String(flag)
+//console.log(q, typeof q); // output -> 1, number
+//console.log(d, typeof d); // output -> true , string
+
+let age = 18
+//console.log(age,typeof age)
+
+let changeString = String(age);
+let g = Boolean(age)
+//console.log(changeString, typeof changeString);
+console.log(g, typeof g); // true ,boolean 
+
+let name = "Vishal"
+let changeNum = Number(name)
+let n = ""
+let w = Boolean(n)
+//console.log(changeNum) // output is NaN -> it means Not a Number
+//console.log(typeof changeNum) // output is -> number
+console.log(w,typeof w); // for empty string it will give false when change 
+
+let name2 = "Vishal123"
+let chg = Number(name2)
+//console.log(chg, typeof chg); // output NaN ,number
+
+let abc = null
+let ch = Number(abc)
+let a  = String(abc)
+//console.log(ch, typeof ch);  // output -> 0, number
+//console.log(a,typeof a); // output-> null ,string
+
+
+let name1 = "varul"
+let temp1 = Boolean(name1)
+//console.log(temp1, typeof temp1);
+
+
