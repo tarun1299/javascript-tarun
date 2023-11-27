eca478b234d4958c76189743c0cf88a50ee72b05
tarun1299 <152164072+tarun1299@users.noreply.github.com>
2023-11-27 12:22:09 +0000

    variables in js

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const empId = 12345
+const company = "HCL"
+let empName = "Vishal"
+var empAddress = "Indore"
+empSalary  = "20000"
+let increment;
+
+
+console.log(empId);
+console.table([empId,empName,company,empAddress, empSalary]); // it will print data in tables 
+
+empName = "Varul"
+empAddress = "Rau"
+empSalary = "25000"
+
+console.table([empId,empName,company,empAddress,empSalary,increment])
+
+
