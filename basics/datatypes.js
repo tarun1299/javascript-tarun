5d7cb8bbe41a2863d33d0b59a018856f87fc80d7
tarun1299 <152164072+tarun1299@users.noreply.github.com>
2023-11-27 12:23:15 +0000

    datatypes in js

---
+++ basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) 
+// we are using nodejs, not browser , alert is use to show the message in browser window 
+
+
+console.log("Tarun")
+
+
+let name = "varun" // string type
+let age = 21    // number type 
+let isPassed = true // boolean type 
+let marks = 7.4 // number 
+let address;  // not assign any value so it is undefined type 
+let status = null  // null
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value , means empty 
+// undefined => 
+// symbol => unique 
+
+
+// object
+console.log(typeof name);
+console.log(typeof(age))
+console.log(typeof marks);
+console.log(typeof isPassed);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
