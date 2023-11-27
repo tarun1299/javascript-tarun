7e6320e908d90227ac0268b67fc14b5882bd3ee4
tarun1299 <152164072+tarun1299@users.noreply.github.com>
2023-11-27 14:30:00 +0000

    operations js

---
+++ basics/operation.js
@@ -0,0 +1,65 @@
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 5 + 4
+
+console.log(num1,num2,num3) //output 9 9 9 
+
+let gameCounter = 100
+++gameCounter;
+//console.log(gameCounter);
+
+//===========================================
+
+let num  = 21
+// console.log(-num)
+
+// console.log(3 + 4)
+// console.log(3 - 4)
+// console.log(3 * 4)
+// console.log(3 / 4)
+// console.log(3 % 4)
+// console.log(3 ** 4)
+
+let str1 = "Tarun"
+let str2 = " Tirole"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("3" + 4)
+console.log(3 + "4")
+console.log("3" + "4")
+console.log("3" + 4 + 4)
+console.log(3 + 4 + "4")
+
+console.log(6 + 4  * 5 % 3) // output 8
+console.log(6 + 4  % 5 * 3) // output 18
+
+
+
+
+
